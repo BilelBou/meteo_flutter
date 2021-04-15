bc51483925e2ae5458c75e132ae0240f444ff0f9
Brackyt <60280126+Brackyt@users.noreply.github.com>
2021-04-15 20:22:09 +0200

    [ADD] Profile page with image picker, saved to storage and loaded

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -3,7 +3,10 @@ import 'dart:io';
 import 'package:meteo_flutter/components/meteo_page.dart';
 import 'package:meteo_flutter/navigationSystem.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ProfilePage extends StatefulWidget {
   @override
@@ -17,18 +20,38 @@ class _ProfilePageState extends State<ProfilePage> {
 
   Future getImage() async {
     final pickedFile = await picker.getImage(source: ImageSource.camera);
+    
+    File tmpFile = File(pickedFile.path);
 
-    setState(() {
-      if (pickedFile != null) {
-        _image = File(pickedFile.path);
-      } else {
-        print('No image selected.');
-      }
-    });
+    final String path = (await getApplicationDocumentsDirectory()).path;
+    final String fileName = basename(pickedFile.path);
+    final String fileExtension = extension(pickedFile.path);
+
+    tmpFile = await tmpFile.copy('$path/$fileName$fileExtension');
+
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString('profile_image', tmpFile.path);
+
+    setState(() => _image = tmpFile);
+  }
+
+  Future<String> getImageFromStorage() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String path = prefs.getString('profile_image');
+
+    if (path != null && path.isNotEmpty) {
+      setState(() {
+        _image = File(path);
+      });
+      
+      return path;
+    }
+    return "";
   }
 
   @override
   Widget build(BuildContext context) {
+    getImageFromStorage();
     return MeteoPage(
       title: "Profile Page",
       children: [
